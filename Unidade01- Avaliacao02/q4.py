e00fa3d9e7ecd67c939c2553e16064e3ca5ef249
༒ Júlio César ༒ <165187201+Juliocesar1704@users.noreply.github.com>
2025-01-01 21:44:08 -0300

    Update q4.py

--- Unidade01- Avaliacao02/q4.py
+++ Unidade01- Avaliacao02/q4.py
@@ -1,1 +1,46 @@
+import random
+palavras = (
+    "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
+    "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
+    "BARRO", "BEIJO", "BICHO", "BORDA", "BORRA", "BRAVO", "BREJO",
+    "BURRO", "CAIXA", "CALDO", "CANJA", "CARRO", "CARTA", "CERVO",
+    "CESTA", "CLIMA", "COBRA", "COLAR", "COQUE", "COURO", "CRAVO",
+    "DARDO", "FAIXA", "FARDO", "FENDA", "FERRO", "FESTA", "FLUOR",
+    "FORCA", "FORNO", "FORTE", "FUNDO", "GAITA", "GARRA", "GENIO",
+    "GESSO", "GRADE", "GRANA", "GRAMA", "GURIA", "GREVE", "GRUTA",
+    "HEROI", "HOTEL", "ICONE", "IMPAR", "IMUNE", "INDIO", "JUNTA",
+    "LAPIS", "LARVA", "LAZER", "LENTO", "LESTE", "LIMPO", "LIVRO",
+    "MACIO", "MAGRO", "MALHA", "MANSO", "MARCO", "METAL", "MORTE",
+    "MORRO", "MURAL", "MOVEL", "NACAO", "NINHO", "NOBRE", "NORMA",
+    "NORTE", "NUVEM", "PACTO", "PALHA", "PARDO", "PARTE", "PEDRA",
+    "PEDAL", "PEIXE", "PRADO", "PISTA", "POMBO", "POETA", "PONTO",
+    "PRATO", "PRECO", "PRESO", "PROSA", "PRUMO", "PULGA", "PULSO",
+    "QUEPE", "RAIVA", "RISCO", "RITMO", "ROSTO", "ROUPA", "SABAO",
+    "SALTO", "SENSO", "SINAL", "SITIO", "SONHO", "SOPRO", "SURDO",
+    "TARDE", "TERNO", "TERMO", "TERRA", "TIGRE", "TINTA", "TOLDO",
+    "TORRE", "TRAJE", "TREVO", "TROCO", "TRONO", "TURMA", "URUBU",
+    "VALSA", "VENTO", "VERDE", "VISAO", "VINHO", "VIUVO", "ZEBRA"
+    )
 
+palavra1 = random.choice(palavras)
+palavra2 = random.choice(palavras)
+palavra3 = "-"*len(palavra1)
+palavra4 = "-"*len(palavra2)
+
+print("Termoo verçao dueto")
+print(f"Tente adivinhar as palavras: {palavra3} {palavra4}")
+
+palavra5 = str(input("Digite uma palavra: "))
+
+tentativas = 7
+
+while(tentativas > 0) and (palavra5 != palavra3) and (palavra5 != palavra4):
+    palavra5 = str(input("Digite uma palavra: "))
+    tentativas -= 1
+    
+    if palavra5 == palavra3 and palavra5 != palavra4:
+        print("Parabéns você acertou a primeira palavra!")
+    elif palavra5 == palavra4 and palavra5 != palavra3:
+        print("Parabéns você acertou a segunda palavra!")
+    elif palavra5 == palavra3 and palavra5 == palavra4:
+        print("Parabéns você acertou as duas palavras!")
