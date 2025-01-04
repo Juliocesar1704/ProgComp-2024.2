e0c34c1703ddcb6c15c7ff0633de6cfc6c1b26e7
༒ Júlio César ༒ <165187201+Juliocesar1704@users.noreply.github.com>
2025-01-04 13:07:16 -0300

    Update q2.py

--- Unidade01- Avaliacao02/q2.py
+++ Unidade01- Avaliacao02/q2.py
@@ -4,7 +4,7 @@
 # Declaramos aqui qual o primeiro número primo e onde devemos começar
 numN = 10
 # Aqui declaramos até onde podemos chegar
-limite = 1000000
+limite = 100000
 # Aqui declaramos a variavel que vai contar a quantidade de palindromos
 contadorP = 0 
 
@@ -29,8 +29,8 @@ for pos in range(numN, limite + 1):
         contadorP += 1 
         # adiciona 1 ao contador
 
-print(f"A quantidade de numeros palindromos entre 10 e 1000000 é : {contadorP}")
-# Aqui exibimos a quantidae de palindromos entre 10 e 1000000
+print(f"A quantidade de numeros palindromos entre 10 e 100000 é : {contadorP}")
+# Aqui exibimos a quantidae de palindromos entre 10 e 100000
 
 """
 Ideias para melhorar o código:
