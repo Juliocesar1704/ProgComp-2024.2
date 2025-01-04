496042963a56e75a458bb96635f190829de612a6
David Douglas Santos Barbosa <d4v1d.soudouglas@gmail.com>
2025-01-04 13:38:53 -0300

    Update q5.py

--- Unidade01- Avaliacao02/q5.py
+++ Unidade01- Avaliacao02/q5.py
@@ -16,7 +16,7 @@ for ano in range(ano_inicial, ano_hoje + 1):
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
         anos_bissextos += 1
 
-# Ele fará as contagens do dias partindo da data primária que a questão 
+# Ele fará as contagens do dias partindo da data primária que a questão determinou até o dia atual. 
 dias_no_ano_inicial = (12 - mes_inicial) * 30 + (30 - dia_inicial)
 dias_no_ano_atual = (mes_hoje - 1) * 30 + dia_hoje
 anos_completos = ano_hoje - ano_inicial - 1
@@ -31,12 +31,12 @@ for dia in range(dias_totais + 1):
     if (dia + 5) % 7 == 0:
         sabados_contados += 1
 
-#Apenas uma questão estética ele não interfere no código, apenas mostra por extenos a data
+#Apenas uma questão estética ele não interfere no código, apenas mostra por extenso a data
 locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
-dia_da_semana = hoje.strftime('%A, %d %B %Y')
+dia_da_semana = hoje.strftime('%A, %d de %B de %Y')
 
 # Exibindo os resultados
 print(f"Data inicial: {ano_inicial}-{mes_inicial}-{dia_inicial}")
 print(f"Desde {ano_inicial}{mes_inicial}{dia_inicial}, na presente data {hoje}, fazem cerca de {dias_totais} dias")
-print("Hoje é:", dia_da_semana)
+print(f"Hoje é ;{dia_da_semana})
 print(f"Se passaram {sabados_contados} sábados até a presente data.")
